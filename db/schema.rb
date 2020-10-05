f80d97c210850b8d7d329423c23280fbe35a11ef
yuya <f-oinuma@hotmail.co.jp>
2020-10-05 11:28:39 +0000

    for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_10_04_113946) do
 
-  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
