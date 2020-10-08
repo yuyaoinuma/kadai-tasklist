7df7691bdea5bb3055ba2f30fd51373a3fa3e265
yuya <f-oinuma@hotmail.co.jp>
2020-10-08 01:37:27 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'toppages/index'
+  get 'users/create'
+  root to: 'toppages#index'
   
   resources :tasks
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
+  get "/", to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_113946) do
-
+ActiveRecord::Schema.define(version: 2020_10_07_142744) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
