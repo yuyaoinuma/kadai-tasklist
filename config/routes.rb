a0f4c139023ad93df45632492b41c4139c7a2273
yuya <f-oinuma@hotmail.co.jp>
2020-10-09 06:04:29 +0000

    kadai-tasklist-login

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
- 
+  
   get 'new_user_path', to: "tasks#index"
   resources :users, only: [:new, :create]
  
