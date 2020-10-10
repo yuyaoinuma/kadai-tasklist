0097070e152adcb3d03431fde8055c3d4bb5fe20
yuya <f-oinuma@hotmail.co.jp>
2020-10-10 07:47:49 +0000

    herokulogin

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,5 @@
 module UsersHelper
-  def gravatar_url(user, options = { size: 80 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-  end
-  
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
